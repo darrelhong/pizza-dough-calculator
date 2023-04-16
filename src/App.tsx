358a96a00431ebbd65f4d05448c80cd5bb376465
darrelhong <darrel.hong@gmail.com>
2023-04-16 11:00:18 +0800

    feat: inital setup with solidjs, tailwind, vite and eslint

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="relative px-6 py-4 h-screen">
+      <div
+        class="absolute inset-x-0 -top-36 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-64"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[40rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#af80ff] to-[#ffd181] opacity-30 sm:w-[54rem]"
+          style={{
+            'clip-path':
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+          }}
+        />
+      </div>
+      <div class="mx-auto max-w-2xl">
+        <h1 class="text-2xl font-bold text-gray-800 text-center">Pizza dough calculator</h1>
+      </div>
+      <div
+        class="absolute inset-x-0 -bottom-10 -z-10 transform-gpu overflow-hidden blur-2xl"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#feac69] to-[#a962fa] opacity-30 sm:-bottom-48 sm:w-[60rem]"
+          style={{
+            'clip-path':
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindcssformsplugin from '@tailwindcss/forms';
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [tailwindcssformsplugin]
+} satisfies Config;
