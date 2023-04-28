79e6796b75ee5f391f522594b3b2ee5509c63d8f
darrelhong <darrel.hong@gmail.com>
2023-04-28 21:38:52 +0800

    feat: add icons and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,34 @@ export default defineConfig({
   plugins: [
     solidPlugin(),
     VitePWA({
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.icon', 'safari-pinned-tab.svg', 'icons/ios.png'],
+      manifest: {
+        name: 'Pizza Dough Calculator',
+        short_name: 'Dough Calc',
+        description: 'An awesome pizza dough calculator',
+        theme_color: '#faae1d',
+        background_color: '#faae1d',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
     })
   ],
   build: {
