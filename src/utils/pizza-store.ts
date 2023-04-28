54a0077004eca7df66b3bed04c82b4951b820f5a
darrelhong <darrel.hong@gmail.com>
2023-04-28 15:51:42 +0800

    feat: use OR operator for default field values instead

--- src/utils/pizza-store.ts
+++ src/utils/pizza-store.ts
@@ -12,10 +12,10 @@ const query = new URLSearchParams(window.location.search);
 
 export const [state, setState] = createStore({
   fields: {
-    pizza_num: query.get('pizza_num') ?? DEFAULT_VALUES['pizza_num'],
-    weight: query.get('weight') ?? DEFAULT_VALUES['weight'],
-    hydration: query.get('hydration') ?? DEFAULT_VALUES['hydration'],
-    salt: query.get('salt') ?? DEFAULT_VALUES['salt']
+    pizza_num: query.get('pizza_num') || DEFAULT_VALUES['pizza_num'],
+    weight: query.get('weight') || DEFAULT_VALUES['weight'],
+    hydration: query.get('hydration') || DEFAULT_VALUES['hydration'],
+    salt: query.get('salt') || DEFAULT_VALUES['salt']
   } as Record<string, string>,
   get ingredients() {
     return calculateIngredients(
