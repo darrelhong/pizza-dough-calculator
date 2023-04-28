5c74429f280346a1205f18cfcd370b2dbded5a95
darrelhong <darrel.hong@gmail.com>
2023-04-28 20:39:58 +0800

    feat: add pwa caching with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate'
+    })
+  ],
   build: {
     target: 'esnext'
   }
